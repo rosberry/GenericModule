f0548cd6db364c639ae9658e6568e4080079a686
ntunin <nik.tyunin.92@mail.ru>
2020-12-17 13:46:09 +0600

    fix swiftlin wornings

--- Example/Classes/AppDelegate.swift
+++ Example/Classes/AppDelegate.swift
@@ -1,8 +1,4 @@
 //
-//  AppDelegate.swift
-//  GenericModuleExample
-//
-//  Created by Nick Tyunin on 17.12.2020.
 //  Copyright © 2020 Rosberry. All rights reserved.
 //
 
@@ -13,9 +9,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var appCoordinator: AppCoordinator = .init()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         appCoordinator.start(launchOptions: launchOptions)
         return true
     }
 }
-
